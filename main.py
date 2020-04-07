adf2dfbfb03326279ec5050df18157fab3fbcfc7
trohit08 <titaniumrohit@gmail.com>
2020-04-07 17:59:00 +0530

    Modified_one Optimizes code

--- main.py
+++ main.py
@@ -16,12 +16,13 @@ def rules():
     1. For each correct answer you will get 5 points.
     2. For each wrong answer you will deduct minus 2.
     3. You must write the whole answer which is already in options.
-    4. There will be question followed by four options, you have to choose right answers.''')
+    4. There will be question followed by four options, you have to choose right answers.
+    5. If your most of the answers are wrong then you will get negative points.''')
 
 #----------------------------------------------------------------------------------------
 
 def play():
-    
+    #defined empty lists
     questions = []
     op1 = []
     op2 = []
@@ -30,21 +31,23 @@ def play():
     answers = []
     #count length of lines in document
     count = len(open('capital_ques.txt').readline())
-    with open('capital_ques.txt','r') as file:  #read file
-        for f in islice(file,1,count):  #sliceed the line from 2nd line to last line
+    with open('capital_ques.txt','r') as file:  #read the file
+        for f in islice(file,1,count):  #sliced the line from 2nd line to last line
             wor = [s.strip() for s in f.split(',')] #remove spaces and split the line from somepoint
             
+            #appended questions, options and answers into empty lists 
             questions.append(wor[0])
             op1.append(wor[1])
             op2.append(wor[2])
             op3.append(wor[3])
             op4.append(wor[4])
             answers.append(wor[5])
-
+    #input from user
     no_of_quiz = int(input('How many question should be there in your quiz = '))
 
+    #print questions with options
     count = 0
-    for i in range(no_of_quiz):
+    for _ in range(no_of_quiz):
         j = random.randint(1,no_of_quiz)
         print('/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////')
         print(questions[j])
@@ -53,28 +56,28 @@ def play():
         print(op3[j])
         print(op4[j])
 
-        ans = input('YOUR ANSWER FROM ABOVE OPTIONS (write it down your whole answer) : ')
+        ans = input('YOUR ANSWER FROM ABOVE OPTIONS (write it down your whole answer) : ')  #user answer
         
-        if ans.lower() == answers[j].lower():
-            count += 5
+        if ans.lower() == answers[j].lower():  #comparing answers in lower case
+            count += 5   #counting points for right answers
             print('\ncorrect')
         else :
-            if no_of_quiz != 1:
-                count -= 2
-                print('\nIncorrect')
-                print('\nCorrect answer is :', answers[j].capitalize())
+            count -= 2  #negative markings for wrong answers
+            print('\nIncorrect')
+            print('\nCorrect answer is :', answers[j].capitalize())
     
-    print('Your Score Is : ',count)
+    print('\nYour Score Is : ',count)
 
 
 #------------------------------------------------------------------------------------------------------------
 
+#initialization point
 print('\n ===============Welcome To The Game================')
 print('\n1. Start Game')
 print('2. Rules')
 print('3. About')
 
-choice = int(input('\n\n ~~~~Enter Your choice :') )
+choice = int(input('\n\n ~~~~Enter Your choice :') )  #input from user
 
 if choice == 1:
     play()
